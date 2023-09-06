38234f0236aecbcf2299209d0bce4fd48daae5fd
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2023-09-06 23:29:45 +0100

    fixed bug with site favicon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SEO from "../common/seo";
-import HomeOne from "../components/homes/home";
+// import HomeOne from "../components/homes/home";
 import Wrapper from "../layout/wrapper";
 import HomeSix from "../components/homes/home-6";
 
